cbee8b124a1abe2e3283c76a425f7076b4d0316a
pratiks14 <pratikshetty14@mail.com>
2019-07-21 02:17:55 +0530

    init spring boot

---
+++ src/test/java/com/springframework/pratikspringpetclinic/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.pratikspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
